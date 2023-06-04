243d95a6d8cb9a24a78dd78de14daab6fa02f82b
Armin Shams <44529358+baraka7723@users.noreply.github.com>
2023-06-04 17:12:22 +0200

    Update ElGamal_bard.py

--- ElGamal_bard.py
+++ ElGamal_bard.py
@@ -1,48 +1,41 @@
 from charm.toolbox.pairinggroup import PairingGroup
 
-
+# Generate key pair
 def crs_gen():
-    """Generate a key pair."""
     group = PairingGroup('curve25519')
     g = group.random()
     sk = group.random()
     h = sk * g
     return (g, h), sk
 
-
+#Add randomness to a point
 def rando(pk, c, r):
-    """Add randomness to a point."""
     return c + r * pk[1]
 
-
+#Encrypt a message with randomness
 def rspeq_enc(pk, m, r):
-    """Encrypt a message with randomness."""
     return r * pk[1] + m
 
-
+#First move of the RSPEQ protocol
 def rspeq_flow_1(pk0, pk1, c0, c1):
-    """First move in the RSPEQ protocol."""
     r1 = group.random()
     r2 = group.random()
     rm = group.random()
     return rando(pk0, (c0.0 + rm, c0.1), r1), rando(pk1, (c1.0 + rm, c1.1), r2), rm, r1, r2
 
-
+##Second move of the RSPEQ protocol
 def rspeq_flow_2():
-    """Second move in the RSPEQ protocol."""
     return group.random()
 
-
+##Third move of the RSPEQ protocol
 def rspeq_flow_3(b, r0, r_0, r1, r_1):
-    """Third move in the RSPEQ protocol."""
     if b:
         return r_0, r_1
     else:
         return r0 + r_0, r1 + r_1
 
-
+#Forth move of the RSPEQ protocol
 def rspeq_flow_4(b, pk0, pk1, c0, c_0, c1, c_1, rx, ry, rm):
-    """Fourth move in the RSPEQ protocol."""
     if b:
         c00 = rando(pk0, (c0.0 + rm, c0.1), rx)
         c11 = rando(pk1, (c1.0 + rm, c1.1), ry)
@@ -50,9 +43,8 @@ def rspeq_flow_4(b, pk0, pk1, c0, c_0, c1, c_1, rx, ry, rm):
     else:
         return c_0.0 - (rx * pk0[1]) == c_1.0 - (ry * pk1[1])
 
-
+#Test the RSPEQ protocol
 def main():
-    """Test the RSPEQ protocol."""
     (pk0, sk0) = crs_gen()
     (pk1, sk1) = crs_gen()
     m0 = group.zero() * pk0[0]
